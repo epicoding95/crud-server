079e3a18ee834004da60c5fea3d8496c5d74003c
kelar crisp <kelcrisp007@icloud.com>
2020-06-04 14:51:55 -0700

    routes setup to the DB with absic validation

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,13 +4,20 @@ const Product = require('../models/product');
 const mongoose = require('mongoose');
 //if you add code after the initial response you need to RETURN the first response
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'handling get reuqest to /products'
+    //find is a static method you can call to retrieve ALL the data from a particualr db
+    Product.find().exec().then(
+        docs => {
+            res.status(200).json(docs)
+        }
+    ).catch(err => {
+        res.status(500).json({
+            error: err
+        })
     })
+
 })
 
 router.post('/', (req, res, next) => {
-
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
@@ -18,39 +25,62 @@ router.post('/', (req, res, next) => {
     });
     //exec turns this into a promise
     product.save().then(result => {
-        console.log(result)
-    }).catch(err => console.log(err));
-    res.status(201).json({
-        message: 'handling POST reuqest to /products',
-        createdProduct: product
+        res.status(201).json({
+            message: 'handling POST reuqest to /products',
+            createdProduct: product
+        })
+    }).catch(err => {
+        console.log(err)
+        res.status(500).json({ error: err })
     })
+
+
 })
 
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
+    //find is a static method you can call to retrieve a single piece of data back
+    Product.findById(id).exec().then(
+        doc => {
+            if (doc) {
+                res.status(200).json(doc)
+            } else {
+                res.status(404).json({ message: 'not a valid id' })
+            }
 
-    if (id === 'special') {
-        res.status(200).json({
-            message: 'you discovered the special Id',
-            id: id
-        })
-    } else {
-        res.status(200).json({
-            message: 'not the special Id'
-        })
-    }
+        }
+    ).catch(err => {
+        console.log(err)
+        res.status(500).json({ error: err })
+    })
 })
 
 router.patch('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    const updateOps = {};
+    //iterate through the updates to create an obj and only edit the properties that are in the updateOps object
+    //the user needs to know they HAVE to send in array with an object containing the property values they want to update
+    for (const options of req.body) {
+        updateOps[options.propName] = options.value;
+    }
+    //the first argument is the id to identify what you want to change, the second argument is the info to change
+    Product.updateOne({ _id: id }, { $set: updateOps }).exec().then(result => {
+        res.status(200).json(result)
+    }).catch(err => {
+        res.status(500).json({ error: err })
+    })
     res.status(200).json({
         message: 'updated product'
     })
 })
 
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'deleted product'
+    const id = req.params.productId;
+    Product.remove({ _id: id }).exec().then(result => {
+        res.status(200).json(result)
+    }).catch(err => {
+        res.status(500).json({ error: err })
     })
 })
 module.exports = router;
